b1abeb41d786d223dd50387f420a75c5b2947782
khs3z <khs3z@virginia.edu>
2021-08-03 23:43:28 -0400

    separate embryos, improved spindle pole tracking, initial DNA tracking

--- mitoanalysis.py
+++ mitoanalysis.py
@@ -11,6 +11,7 @@ import glob
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+from scipy.ndimage import label
 from nd2reader import ND2Reader
 
 def get_files(path, fpattern='*.tif'):
@@ -82,7 +83,7 @@ def square_edges(edges):
         scorners.append([p1,p2,p3,p4])
     return sedges,scorners
     
-def process_spindle(image):
+def process_spindle(image, polesize=20):
     height,width = image.shape
     #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     #image = clahe.apply(image)
@@ -92,40 +93,60 @@ def process_spindle(image):
     ret1,binary = cv2.threshold(blurredimg,191,255,cv2.THRESH_BINARY)
     
     sp_contours,hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    print (f'\tcontours: {len(sp_contours)}')
     if len(sp_contours) == 0:
         return image,binary,np.array([(0,0),(0,0)]) 
+    sp_contours.sort(key=cv2.contourArea, reverse=True)
+    if len(sp_contours) > 2:
+        sp_contours = sp_contours[:2]
+    #for c in sp_contours:
+    #    print (f'\tarea={cv2.contourArea(c)}')
+    #   print (f'\tcenter={get_centers(c)}')
+    binary = np.zeros((width,height),np.uint8)
+    cv2.fillPoly(binary, sp_contours, (255,255,255))
     poles = [get_centers(c) for c in sp_contours]
     if len(sp_contours) > 1:
         color = (255,255,255)
         thickness = 2
         binary = cv2.line(binary, poles[0], poles[1], color, thickness)
         sp_contours,hierarchy = cv2.findContours(binary, 1, 2)
+    #print (f'\tlen(sp_contours): {len(sp_contours)}')
     corners = get_rect_points(sp_contours[0])
-    print (f'corners: {corners}', type(corners[0]))
+    #print (f'\tcorners: {corners}', type(corners[0]))
     edges = get_edges(corners)
-    print (f'edges: {edges}')
+    #print (f'\tedges: {edges}')
     edges,scorners = square_edges(edges[:2])
-    print (f'square corners: {scorners}')
+    #print (f'\tsquare corners: {scorners}')
     poles = []
+    binary = np.zeros((height,width),np.uint8)
     for square in scorners[:2]:
         mask = np.zeros((height,width),dtype=np.uint8)
         square = np.array(square).flatten().reshape((len(square),2))
-        print (square)
-        cv2.fillPoly(mask, pts = [square], color =(255,255,255))
+        #print (f'\tsquare={square}')
+        cv2.fillPoly(mask, pts =[square], color=(255,255,255))
         img = cv2.bitwise_and(blurredimg,blurredimg,mask = mask)
-        ret1,binary = cv2.threshold(img,191,255,cv2.THRESH_BINARY)    
-        sp_contours,hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        ret1,tmpbinary = cv2.threshold(img,191,255,cv2.THRESH_BINARY)    
+        sp_contours,hierarchy = cv2.findContours(tmpbinary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.fillPoly(binary, pts=sp_contours, color=(255,255,255))
         poles.append(get_centers(sp_contours[0]))
 
     #image = draw_lines(image,edges,color=(255,0,0),thickness=1)
-    print (f'spindle poles: {poles}')
+    #print (f'\tspindle poles: {poles}')
     return image,binary,poles #spindle_poles
 
     #ret2,binary2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-def process_dna(img):
-    return img,[],[]
+def process_dna(image):
+    chromatids = []
+    height,width = image.shape
+    blurredimg = cv2.medianBlur(image,3,0)
+    ret1,binary = cv2.threshold(blurredimg,127,255,cv2.THRESH_BINARY)
+    #binary = cv2.adaptiveThreshold(blurredimg, 255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 10)
+    
+    dna_contours,hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    binary = np.zeros((width,height), np.uint8)
+    cv2.fillPoly(binary, pts=dna_contours,color=(255,255,255))
+    chromatids = [get_centers(c) for c in dna_contours]
+    return image,binary,chromatids
 
 def save_movie(images, fname, codec='mp4v'):
     height,width,_ = images[0].shape
@@ -137,60 +158,119 @@ def save_movie(images, fname, codec='mp4v'):
     vout.release()
     return success
 
+def watershed(a, img):
+    border = cv2.dilate(img, None, iterations=5)
+    border = border - cv2.erode(border, None)
+
+    dt = cv2.distanceTransform(img, cv2.DIST_L2, 3)
+    dt = ((dt - dt.min()) / (dt.max() - dt.min()) * 255).astype(np.uint8)
+    _, dt = cv2.threshold(dt, 159, 255, cv2.THRESH_BINARY)
+    lbl, ncc = label(dt)
+    lbl = lbl * (255 / (ncc + 1))
+    # Completing the markers now. 
+    lbl[border == 255] = 255
+
+    lbl = lbl.astype(np.int32)
+    odt = lbl.astype(np.uint8)
+    markers = cv2.watershed(a, lbl)
+
+    lbl[lbl == -1] = 0
+    lbl = lbl.astype(np.uint8)
+    masks = []
+    print (f'ncc={ncc}, unique={np.unique(lbl)}')
+    for i in np.unique(lbl):
+        if i == 0 or i == 255:
+            continue
+        mask = np.zeros(img.shape,np.uint8)
+        mask[lbl==i] = 255
+        masks.append(mask)
+    return odt, 255 - lbl, masks
+    
 def process_file(fname):
     with ND2Reader(fname) as imagestack:
         print (imagestack.sizes)
         print (imagestack.axes)
         print (imagestack.frame_shape)
-        poles = []
-        spimages = []
-        dnaimages = []
-        imagestack.iter_axes = 'tc'
+        if imagestack.sizes['c'] < 2:
+            print ("Skipping -- not enough channels.")
+            return
         width = imagestack.sizes['x']
         height = imagestack.sizes['y']
-        print (imagestack.frame_shape)
-        for i,img in enumerate(imagestack):
-            print (f'i={i}, img.shape={img.shape}')
-            # img =  cv2.normalize(img, None, 0, int(255.0*img.max()/65535.0), norm_type=cv2.NORM_MINMAX)
-            img = cv2.normalize(img, None, 0, 255, norm_type=cv2.NORM_MINMAX)
-            img = np.uint8(img)
+        
+        imagestack.bundle_axes = 'cxy'
+        imagestack.iter_axes = 't'
+        #max_spindle_int = np.amax(np.array(imagestack)[:,1])
+        #max_dna_int = np.amax(np.array(imagestack)[:,0])
 
-            if i % 2 == 0:
-                dnaimg,binary,dnablobs = process_dna(img)
-                dnaimages.append(dnaimg)
-            else:     
-                spimg,binary,spindle_poles = process_spindle(img)
-                poles.append(spindle_poles)
-                spimages.append(spimg)
-        blue = np.zeros((width,height), np.uint8)
-        images = [cv2.merge([blue,spimages[i], dnaimages[i]]) for i in range(len(spimages))]
-        for i,frame_poles in enumerate(poles):
-            for p in frame_poles:
-                images[i] = cv2.circle(images[i], (p[0],p[1]), 4, (255,0,255), 1)
-        #plt.subplot(121)
-        #plt.imshow(spimg, cmap='gray')
-        #plt.subplot(122)
-        #plt.imshow(binary, cmap='gray')
-        poles = np.array(poles)
-        sp_centers = [center(p[0], p[1]) for p in poles] 
-        pole1_distance = [euclidian(p1=poles[i][0], p2=sp_centers[i]) for i in range(len(poles))]
-        pole2_distance = [-euclidian(p1=poles[i][1], p2=sp_centers[i]) for i in range(len(poles))]
-        #plt.plot(pole1_distance, color='green')
-        #plt.plot(pole2_distance, color='magenta')
-        #plt.show()
+        med = np.median(np.array(imagestack)[:,1], axis=0)
+        med = cv2.normalize(med, None, 0, 255.0, norm_type=cv2.NORM_MINMAX)
+        med = med.astype(np.uint8)
+        __,embryo = cv2.threshold(med,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        kernel = np.ones((3,3),np.uint8)
+        embryo = cv2.morphologyEx(embryo,cv2.MORPH_CLOSE, kernel, iterations = 1)
+        dt, embryo, masks = watershed(cv2.merge([embryo,embryo,embryo]),embryo)
+        
+        blank = np.zeros((height,width), np.uint8)
+
+
+        for embryo_no,embryo in enumerate(masks):
+            allpoles = []
+            allchromatids = []
+            spimages = []
+            dnaimages = []
+            for frame in imagestack:
+                spimg = frame[1]
+                spimg = cv2.normalize(spimg, None, 0, 255.0, norm_type=cv2.NORM_MINMAX)
+                spimg = np.uint8(spimg)
+                spimages.append(spimg) # spimg
+                #frame_int = np.amax(spimg)
+                spimg = cv2.bitwise_and(spimg,spimg,mask = embryo)
+                spimg = cv2.normalize(spimg, None, 0, 255.0, norm_type=cv2.NORM_MINMAX)
+                spimg,binary,spindle_poles = process_spindle(spimg)
+                allpoles.append(spindle_poles)
     
-        moviefile = os.path.splitext(fname)[0] + '.mp4'
-        save_movie(images, moviefile)    
-        cv2.imshow('image',spimg)
+                dnaimg = frame[0]
+                dnaimg = cv2.normalize(dnaimg, None, 0, 255, norm_type=cv2.NORM_MINMAX)
+                dnaimg = np.uint8(dnaimg)
+                dnaimages.append(dnaimg)
+                
+                dnaimg = cv2.bitwise_and(dnaimg,dnaimg,mask = embryo)
+                dnaimg = cv2.normalize(dnaimg, None, 0, 255, norm_type=cv2.NORM_MINMAX)
+                dnaimg,binary,chromatids = process_dna(dnaimg)
+                allchromatids.append(chromatids)
+                
+            #blue = np.zeros((width,height), np.uint8)
+            images = [cv2.merge([blank, spimages[i], dnaimages[i]]) for i in range(len(spimages))]
+            for i,frame_poles in enumerate(allpoles):
+                for p in frame_poles:
+                    images[i] = cv2.circle(images[i], (p[0],p[1]), 4, (255,0,255), 1)
+            for i,frame_chromatids in enumerate(allchromatids):
+                for c in frame_chromatids:
+                    images[i] = cv2.circle(images[i], (c[0],c[1]), 4, (255,255,0), 1)
+            #plt.subplot(121)
+            #plt.imshow(spimg, cmap='gray')
+            #plt.subplot(122)
+            #plt.imshow(binary, cmap='gray')
+            allpoles = np.array(allpoles)
+            sp_centers = [center(p[0], p[1]) for p in allpoles] 
+            pole1_distance = [euclidian(p1=allpoles[i][0], p2=sp_centers[i]) for i in range(len(allpoles))]
+            pole2_distance = [-euclidian(p1=allpoles[i][1], p2=sp_centers[i]) for i in range(len(allpoles))]
+            #plt.plot(pole1_distance, color='green')
+            #plt.plot(pole2_distance, color='magenta')
+            #plt.show()
+        
+            moviefile = os.path.splitext(fname)[0] + f'-embryo-{(embryo_no+1):04d}.mp4'
+            save_movie(images, moviefile)    
+            #print (f'max_spindle_int={max_spindle_int}, max_dna_int={max_dna_int}')
 
 def main():
-    files = get_files('data',fpattern='*.nd2')
+    #files = get_files(os.path.join('data','prblem'),fpattern='*.nd2')
+    files = get_files(os.path.join('data'),fpattern='*.nd2')
     print (files)
     for fname in files:
         print (f'Processing: {fname}')
         process_file(fname)
-    plt.show()
-    cv2.waitKey()
+    #plt.show()
   
 
 
